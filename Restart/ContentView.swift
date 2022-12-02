5eb6508a0fe3c5fcbedc458658ea2bb52fba1942
Olha Khomlyak <olga.khomlyak@gmail.com>
2022-12-02 13:44:14 +0300

    Initial Commit

---
+++ Restart/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Olha Khomlyak on 2.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Olha Khomlyak on 2.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
